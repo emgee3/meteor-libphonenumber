8b90cc6eacfa0f83b26fad1beb197129ba177c8d
emgee3 <hello@gravitronic.com>
2014-11-14 23:01:20 -0800

    shorten description

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'emgee:libphonenumber',
-  summary: '(wrapper) Javascript library for parsing, formatting, storing and validating international phone numbers',
+  summary: '(wrapper) Library for parsing, formatting, storing and validating international phone numbers',
   version: '0.1.0',
   git: 'https://github.com/emgee3/meteor-libphonenumber.git'
 });
